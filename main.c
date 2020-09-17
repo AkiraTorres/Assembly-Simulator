0c7ca640c7f0738f6ad3f06153d7ff9e24a89632
PegasusAkira <bluemakco@gmail.com>
2020-09-17 06:57:40 -0300

    created add function

--- main.c
+++ main.c
@@ -5,6 +5,24 @@
 #include <string.h>
 #include <strings.h>
 
+//*
+int *fodase(int *A, int *B, int *C, int *D) {
+    int i;
+    int *pointers = (int *)malloc(4 * sizeof(int));
+    for (i = 0; i < 4; i++) {
+        if (i == 0) {
+            pointers[i] = *A;
+        } else if (i == 1) {
+            pointers[i] = *B;
+        } else if (i == 2) {
+            pointers[i] = *C;
+        } else if (i == 3) {
+            pointers[i] = *D;
+        }
+    }
+    return pointers;
+}
+
 //* funcao assembly MOV
 void mov(char *destiny, char *data, int *dataMemory, int *A, int *B, int *C, int *D) {
     int i;
@@ -98,6 +116,70 @@ void mov(char *destiny, char *data, int *dataMemory, int *A, int *B, int *C, int
     }
 }
 
+//* funcao assembly ADD
+void add(char *destiny, char *data, int *dataMemory, int *A, int *B, int *C, int *D) {
+    int i, j;
+    char *addr;
+    int *cons;
+
+    //* faz o ADD de um registrador para um registrador
+    if (((data[0] == 'A') || (data[0] == 'B') || (data[0] == 'C') || (data[0] == 'D'))) {
+        char regis = destiny[0];
+        int *pointers = fodase(A, B, C, D);
+        int parcel;
+        int count = 0;
+        char k = 65;
+
+        for (i = 65; i < 69; i++) {
+            if (data[0] == i) {
+                printf("%i", count);
+                parcel = pointers[count];
+            }
+            count++;
+            k++;
+        }
+
+        switch (regis) {
+            case 'A':
+                *A += parcel;
+                break;
+
+            case 'B':
+                *B += parcel;
+                break;
+            case 'C':
+                *C += parcel;
+                break;
+            case 'D':
+                *D += parcel;
+                break;
+        }
+    }
+    //* faz o ADD de uma constante para um registrador
+    else if ((destiny[0] == 'A') || (destiny[0] == 'B') || (destiny[0] == 'C') || (destiny[0] == 'D')) {
+        char regis = destiny[0];
+        for (i = 0; i < strlen(data); i++) {
+            addr[i] = data[i];
+        }
+        int cons = atoi(addr);
+        switch (regis) {
+            case 'A':
+                *A += cons;
+                break;
+
+            case 'B':
+                *B += cons;
+                break;
+            case 'C':
+                *C += cons;
+                break;
+            case 'D':
+                *D += cons;
+                break;
+        }
+    }
+}
+
 //* imprimir o estado dos Registradores e da Memoria de Dados
 void print(int A, int B, int C, int D, int *dataMemory) {
     int i;
@@ -174,7 +256,7 @@ int main() {
         if (strncmp(function, "MOV", 3) == 0) {
             mov(param1, param2, dataMemory, &A, &B, &C, &D);  //* se a function for igual a MOV ele chama a funcao mov
         } else if (strncmp(function, "ADD", 3) == 0) {
-            printf("add");
+            add(param1, param2, dataMemory, &A, &B, &C, &D);  //* se a function for igual a ADD ele chama a funcao add
         } else if (strncmp(function, "HLT", 3) == 0) {
             stop = false;  //* se a function for igual a HLT entao o codigo e encerrado
         }
